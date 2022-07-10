c634382965f3a1d597737b5d83249150a6dd24d2
web3ten0 <web3ten0@gmail.com>
2022-07-10 17:04:44 +0900

    added storage and mutation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,99 @@
 use async_graphql::*;
+use std::sync::{Arc, Mutex};
+use uuid::Uuid;
 
 struct Query;
 
 #[Object]
 impl Query {
-    async fn add(&self, a: i32, b: i32) -> MyObject {
-        MyObject { a, b }
+    async fn books(&self, ctx: &Context<'_>) -> Vec<Book> {
+        let storage = ctx.data_unchecked::<Arc<Mutex<Storage>>>().lock().unwrap();
+        storage.books.clone()
+    }
+    async fn authors(&self, ctx: &Context<'_>) -> Vec<Author> {
+        let storage = ctx.data_unchecked::<Arc<Mutex<Storage>>>().lock().unwrap();
+        storage.authors.clone()
     }
 }
 
-#[derive(SimpleObject)]
-struct MyObject {
-    a: i32,
-    b: i32,
+struct Mutation;
+#[Object]
+impl Mutation {
+    async fn add_book(&self, ctx: &Context<'_>, book_name: String) -> Book {
+        let mut storage = ctx.data_unchecked::<Arc<Mutex<Storage>>>().lock().unwrap();
+        let new_book = Book {
+            id: Uuid::new_v4().into(),
+            name: book_name,
+            author: None,
+        };
+        storage.books.push(new_book.clone());
+        new_book
+    }
+    async fn add_author(&self, ctx: &Context<'_>, author_name: String) -> Author {
+        let mut storage = ctx.data_unchecked::<Arc<Mutex<Storage>>>().lock().unwrap();
+        let new_author = Author {
+            id: Uuid::new_v4().into(),
+            name: author_name,
+            books: None,
+        };
+        storage.authors.push(new_author.clone());
+        new_author
+    }
+}
+
+#[derive(SimpleObject, Clone)]
+struct Book {
+    id: ID,
+    name: String,
+    author: Option<Author>,
+}
+#[derive(SimpleObject, Clone)]
+struct Author {
+    id: ID,
+    name: String,
+    books: Option<Vec<Book>>,
+}
+
+struct Storage {
+    books: Vec<Book>,
+    authors: Vec<Author>,
+}
+impl Storage {
+    fn new() -> Self {
+        Self {
+            books: vec![],
+            authors: vec![],
+        }
+    }
 }
 
 #[tokio::main]
 async fn main() {
-    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    let mut _storage = Storage::new();
+    let book = Book {
+        id: Uuid::new_v4().into(),
+        name: "book1".to_string(),
+        author: None,
+    };
+    _storage.books.push(book);
+    let storage = Arc::new(Mutex::new(_storage));
+    let schema = Schema::build(Query, Mutation, EmptySubscription)
+        .data(storage)
+        .finish();
     println!("{}", &schema.sdl());
-    let res = schema.execute("{add(a: 10, b:20) {a}}").await;
+
+    let query = "query {books { name }}";
+    let res = schema.execute(query).await;
+    let res_json = serde_json::to_string(&res);
+    println!("{}", res_json.unwrap());
+
+    let query = "mutation {addBook(bookName:\"book2\") { id name }}";
+    let res = schema.execute(query).await;
+    let res_json = serde_json::to_string(&res);
+    println!("{}", res_json.unwrap());
+
+    let query = "query {books { id name }}";
+    let res = schema.execute(query).await;
     let res_json = serde_json::to_string(&res);
     println!("{}", res_json.unwrap());
 }
