2b50cc740ff263ccd1f561ff2a22ed988068ce71
web3ten0 <web3ten0@gmail.com>
2022-07-10 12:12:55 +0900

    added add query

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
+
+struct Query;
+
+#[Object]
+impl Query {
+    async fn add(&self, a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let schema = Schema::new(Query, EmptyMutation, EmptySubscription);
+    let res = schema.execute("{add(a: 10, b:20)}").await;
+    let res_json = serde_json::to_string(&res);
+    println!("{}", res_json.unwrap());
 }
